ec3ef9f374fa249aaec88839446b47cfe9d94c70
Dhanuka Edirisinghe <dhanuka_edirisinghe@outlook.com>
2023-07-27 10:18:43 +0100

    comitting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LiveBitcoinPrice from './LiveBitcoinPrice';
+
+function App() {
+  return (
+    <div>
+      <h1>Live Bitcoin Price</h1>
+      <LiveBitcoinPrice />
+    </div>
+  );
+}
+
+export default App;
